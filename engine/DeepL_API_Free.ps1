45a46f08c5f05d180034c2c44bfd6a65541bbcbe
MayaEnomoto <maya.enomoto@as-key.com>
2022-04-08 19:51:25 +0900

    1st commit TRALE ps engine files & template files.

---
+++ engine/DeepL_API_Free.ps1
@@ -0,0 +1,26 @@
+Param(
+  [String]$source_language = "",
+  [String]$target_language = "",
+  [String]$source_text = "",
+  [String]$auth_key = ""
+  )
+If ($source_language -eq $target_language){
+  $source
+  return
+}
+
+$Byte = [System.Convert]::FromBase64String($source_text)
+$source = [System.Text.Encoding]::UTF8.GetString($Byte)
+
+If($source_language -eq "AUTO"){
+  $ret = Invoke-WebRequest -Body @{ auth_key="$($auth_key)"; text="$($source)"; target_lang="$($target_language)" } https://api-free.deepl.com/v2/translate
+}Else{
+  $ret = Invoke-WebRequest -Body @{ auth_key="$($auth_key)"; text="$($source)"; source_lang="$($source_language)"; target_lang="$($target_language)" } https://api-free.deepl.com/v2/translate
+}
+
+$json = ConvertFrom-Json $([System.Text.Encoding]::UTF8.GetString( [System.Text.Encoding]::GetEncoding("ISO-8859-1").GetBytes($ret.Content)))
+#$json = ConvertFrom-Json $([System.Text.Encoding]::GetEncoding("Shift_JIS").GetString( [System.Text.Encoding]::GetEncoding("ISO-8859-1").GetBytes($ret.Content)))
+
+$Byte = [System.Text.Encoding]::UTF8.GetBytes($json.translations.text)
+$Base64 = [System.Convert]::ToBase64String($Byte)
+$Base64
---
+++ engine/MicrosoftTranslator.ps1
@@ -0,0 +1,34 @@
+Param(
+  [String]$source_language = "",
+  [String]$target_language = "",
+  [String]$source_text = "",
+  [String]$auth_key = "",
+  [String]$region = ""
+  )
+If ($source_language -eq $target_language){
+  $source
+  return
+}
+
+$Byte = [System.Convert]::FromBase64String($source_text)
+$source = [System.Text.Encoding]::UTF8.GetString($Byte)
+
+$endpoint = 'https://api.cognitive.microsofttranslator.com'
+
+$path = '/translate?api-version=3.0'
+$params = '&from=' + $source_language + '&to=' + $target_language
+$constructed_url = $endpoint + $path + $params
+
+$headers = @{}
+$headers.Add("Ocp-Apim-Subscription-Key",$auth_key)
+$headers.Add("Content-Type","application/json")
+$headers.Add("Ocp-Apim-Subscription-Region",$region)
+
+$Byte = [System.Text.Encoding]::UTF8.GetBytes("[{'Text':'$($source)'}]")
+$ret = Invoke-WebRequest -Uri $constructed_url -Headers $headers -Body $Byte -Method Post 
+
+$json = ConvertFrom-Json $([System.Text.Encoding]::UTF8.GetString( [System.Text.Encoding]::GetEncoding("ISO-8859-1").GetBytes($ret.Content)))
+
+$Byte = [System.Text.Encoding]::UTF8.GetBytes($json.translations.text)
+$Base64 = [System.Convert]::ToBase64String($Byte)
+$Base64
